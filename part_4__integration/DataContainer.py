71819bf8b639d6f4aab103b3f2b50dbda9f7baa2
rachel6854 <rachel6854@gmail.com>
2021-03-09 02:56:20 +0200

    upload Mobileye Project

---
+++ part_4__integration/DataContainer.py
@@ -0,0 +1,8 @@
+class DataContainer:
+    def __init__(self, img_path, traffic_light, EM = None):
+        self.img = img_path
+        self.EM = EM
+        self.auxialary = []
+        self.tfls = []
+        self.candidates = []
+        self.traffic_light = traffic_light
